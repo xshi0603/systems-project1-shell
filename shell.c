e1102bf90fe5fec20f0a10eb03dd68939992c30e
Xing <xshi@stuy.edu>
2017-12-03 22:28:06 -0500

    mostly working

--- shell.c
+++ shell.c
@@ -1,119 +1,155 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <time.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include "shell.h"
-#define READ 0
-#define WRITE 1
-
 
 char ** parse_args( char * line, char * parse_for ) {
     char** args = (char **)calloc(6, sizeof(char *));
     int i;
     for(i = 0; line; i++) {
         args[i] = strsep(&line, parse_for);
+	args[i] = trim(args[i]);
     }
     args[i] = NULL;
     return args;
 }
 
 int run( char * a, char **b ) {
-
-  //char** args = (char **)calloc(6, sizeof(char *));
-  int i;
-  int i2;
-  for(i = 0; b[i]; i++) {
-    printf("b[%d]: %s \n", i, b[i]);
-
-    if (!strcmp(b[i], ";")) {
-      
-      if (!fork()) { //child process
-	printf("need to split\n");
-	char **secondHalf = &b[i + 2];
-	printf("secondhalf[i] = %s", secondHalf[i]);
-	//return(run(secondHalf[i], secondHalf));
-	return 1;
-      }
-      
-    }
-    
-  }
   
-  /*
-    if (!strcmp(b[i], ";")) {
-      //printf("there is a ; :/ \n");
-      break;
-    }      
-    */
-  
-  if (!strcmp(a, "exit")) {
-    return 10; //10 for exit
-  }
-  else if (!strcmp(a, "cd")) {
-    return 11; //11 for cd
-  }
   execvp(a, b);
+  exit(0);
+  
 }
 
-void cd( char *command[]) {
+void exit_cmd( char *command) {
 
-  char *directory = command[1];
-  //int d_ret;
-  //d_ret = chdir(directory);
-  chdir(directory);
+  if (!strcmp(command, "exit")) {
+    exit(0);
+  }
+
+}
+
+void cd( char **command) {
+
+  if (!strcmp(command[0], "cd") && command[1]) {
+    chdir(command[1]);
+  }
   
+}
+
+char * trim(char *command) {
+
+  char * end = command + strlen(command) - 1;
+  
+  while (*command && isspace(*command)) {
+    command++;
+  }
+
+  while (isspace(*end)) {
+    *end-- = '\0';
+  }
+  
+  return command;
+
+}
+
+void redirect_out(char *args[256]) {
+    int i;
+    for (i = 0; args[i]; i++) {
+
+      int new_file;
+
+
+      if (args[i][0] == '>') {
+	char* file_name = args[i+1];
+	new_file = open(file_name, O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	dup2(new_file, 1);
+	close(new_file);
+	args[i] = 0;
+      }
+
+    }
+}
+
+int piping(char *args[256]) {
+    int counter;
+    int ran = -1;
+    
+    char *command = (char*)calloc(256, sizeof(char));
+
+    for (counter = 0; args[counter]; counter++){//finding |
+        int counter2;
+        for (counter2 = 0; args[counter][counter2]; counter2++) {
+            if (args[counter][counter2] == '|') {
+                ran = 0;
+            }
+        }
+        strcat(command,args[counter]);
+    }
+
+    if (!ran) {
+        FILE *pipe;
+        pipe = popen(command, "w");
+        char buf[256];
+        fgets(buf, sizeof(buf), pipe);
+        buf[sizeof(buf) - 1] = 0;
+        pclose(pipe);
+    }
+
+    return ran;
+}
+
+void execute() {
+
+  char *commands = malloc(256);
+  
+  printf("Input a command:\n");
+  fgets(commands, 256, stdin);
+
+  size_t len = strlen(commands); 
+  
+  if(commands[len - 1] == '\n'){
+    commands[len - 1] = '\0';
+  }
+
+  char **args = parse_args(commands, ";");
+  
+  int counter = 0;
+  while (args[counter]) {
+
+    char * command = trim(args[counter]);
+    
+    exit_cmd(command);
+    
+    char ** args = parse_args(command, " ");
+    
+    cd(args);
+
+    int piped = piping(args);
+    
+    if (piped) {
+    
+      if (!fork()) { //child process
+
+	redirect_out(args);
+	run(args[0], args);
+
+      }
+     }
+    //parent process
+
+    int status;
+    int cpid = waitpid(-1, &status, 0);
+    counter++;
+      
+  }
+
+  free(commands);
+  
+  return;
 }
 
 int main(int argc, char *argv[]) {
 
-  char command[256];
-  char **args;
-  
-  int status;
-  int cpid;
-  unsigned char *ret;
-  
-  int fds[2];
-  pipe(fds);
-
-  int i;
-  int counter;
-  
   while (1) {
-    printf("Input a command:\n");
-    fgets(command, 256, stdin);
-
-
-    //printf("args = %s\n", args[0]);
-    args = parse_args(command, "\n");//removing newlines from fgets retval
-    args = parse_args(command, " "); //parses through the string by " "
-    //args = parse_args(command, ";"); //parses through the string by ;
-    
-    //while(args[counter]) {
-	 
-    if (!fork()) { //child process
-      //close(fds[READ]);
-      return(run(args[0], args));
-      //write(fds[WRITE], &i, 4);
-    }
-    else { //parent process
-      //close(fds[WRITE]);
-      //read(fds[READ], &i, 4);
-      //printf("%d", i);
-      cpid = waitpid(-1, &status, 0);
-      ret = (unsigned char *) &status;
-      ret++;
-      
-      if (*ret == 10) {//exit
-	exit(SIGSTOP);
-      }
-      else if (*ret == 11) {//cd
-	cd(args);
-      }
-    }
+    execute();
   }
         
   return 0;
--- shell.h
+++ shell.h
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <signal.h>
+#include <errno.h>
+#include <ctype.h>
+
 char ** parse_args( char *, char *);
+int run( char * , char ** );
+void exit_cmd( char *);
+void cd( char **);
+char * trim(char *);
+void redirect_out(char *args[256]);
+int piping(char *args[256]);
+void execute();
